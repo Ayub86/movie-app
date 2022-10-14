0eb364aaf25bfa6ea21089dbac4dbc4f32564468
Ayub86 <muhammadaayub6@gmail.com>
2022-10-14 17:48:39 +0500

    Routes Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/container/Home/Home";
+import LogIn from "./components/container/Login/Login";
+import Register from "./components/container/Register/Register";
+import ForgotPassword from "./components/container/Forgotpassword/Forgotpassword";
+import ResetPassword from "./components/container/ResetPassword/Reset";
+import Verification from "./components/container/Verification/Verification";
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:otp" element={<ResetPassword />} />
+          <Route path="/verification" element={<Verification />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
