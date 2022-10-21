117bbd2623a78a4aee3f6bdd5fed8237af86417b
Uzair <uzair.mashhadi@gmail.com>
2022-10-21 12:02:32 +0500

    21-oct-update

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,22 +1,23 @@
 import "../../assest/scss/Details.scss"
 
 const Details = (props) => {
-    // const { data } = props
+    // const  {data}  = props
     // console.log("data000",data);
     // const title=props.title
-    console.log(props.title)
+    console.log("props--->",props)
 
-    // const { title, sub_titles, cover_photo, description, generes, languages, rating, createdAt } = props.data
+    // const { title, sub_titles, cover_photo, description, generes, languages, rating, createdAt } = props
+    // console.log(title);
     //  console.log(props.data.title,"title");
     return (
         // <div className="movie-section">
         //     {/* {Object.keys(data).length === 0 ? (
         //         <div>...Loading</div>
 
-        //     ) : ( */}s
+        //     ) : ( */}
         <>
             <div className="section-left">
-                {/* <div className="movie-title">{"title"}</div> */}
+                <div className="movie-title">{props.title}</div>
                 {/* <div className="movie-rating">
                     <span>
                         IMDB Rating <i className="fa fa-star"></i> : {rating}
